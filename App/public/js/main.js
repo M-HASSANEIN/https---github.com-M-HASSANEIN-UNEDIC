f960ef230451a74720bc4f02d4af014276b17a90
mohamed hassanein <momed999@hotmail.com>
2021-10-22 00:59:05 +0200

    css update

--- App/public/js/main.js
+++ App/public/js/main.js
@@ -2,14 +2,49 @@
 
 let button;
 document.addEventListener('DOMContentLoaded', function () {
-
     function delestudent(event) {
         event.preventDefault();
         const id = event.target.getAttribute("data-id")
         console.log(id)
-        if (confirm("Are you sure you want to delete this student ?")) {
-            window.location = this.href
-        }
+
+        let overlay = document.getElementById("overlay");
+        console.log(overlay)
+        let modal = document.querySelector(".modal");
+
+        overlay.classList.add('visible');
+        modal.style.display = "block";
+
+        let close = document.querySelector(".close");
+        close.addEventListener("click", () => {
+            overlay.classList.remove('visible');
+            modal.style.display = "none";
+        });
+        let cancel = document.getElementById("cancel");
+        cancel.addEventListener("click", () => {
+            overlay.classList.remove('visible');
+            modal.style.display = "none";
+        });
+
+        let yes = document.getElementById("yes");
+        yes.addEventListener("click", () => {
+            window.location = this.href;
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+        /* 
+                if (confirm("Are you sure you want to delete this student ?")) {
+                    window.location = this.href
+                } */
     }
 
     button = document.querySelectorAll(".delete-student");
